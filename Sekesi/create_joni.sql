3b3450cb2da3083df619ca731d1ad9654c2912d2
Joep Barmentlo <JoepB@example.com>
2022-06-30 21:29:52 +0200

    Basic structure

---
+++ Sekesi/create_joni.sql
@@ -0,0 +1,8 @@
+INSERT INTO USERS (username, firstName, mail, password)
+VALUES ('jhonny', 'fn', 'jhonny@gmail.com', 'password');
+
+INSERT INTO USERS (username, firstName, mail, password)
+VALUES ('deya', 'dede', 'dede@gmail.com', 'password');
+
+INSERT INTO USERS (username, firstName, mail, password)
+VALUES ('gijsou', 'gigs', 'gigs@gmail.com', 'password');
